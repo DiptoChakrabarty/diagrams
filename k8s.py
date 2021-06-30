f9b30234801037a5b2f3d1a4afe134620d5215cc
DiptoChakrabarty <diptochuck123@gmail.com>
2021-06-30 10:49:16 +0530

    add variable names

--- k8s.py
+++ k8s.py
@@ -1,10 +1,17 @@
-from diagrams import Diagram
+from diagrams import Diagram,Cluster
 from diagrams.k8s.network import Ingress,Service
 from diagrams.k8s.compute import Deployment,Pod,ReplicaSet
+from diagrams.k8s.clusterconfig import HPA
 
 
 with Diagram("K8s",show=True,direction="TB"):
     net = Ingress("example.com") >> Service("svc")
-    net >> Deployment("backend") >> ReplicaSet("rs") >> [Pod("pod1"),
-                               Pod("pod2"),
-                               Pod("pod3") ]
+    deploy = Deployment("backend")
+    replicaset = ReplicaSet("rs")
+    hpa = HPA("hpa")
+    net >> deploy
+    with Cluster("Pod Cluster"):
+        pods = [Pod("pod1"),
+        Pod("pod2"),
+        Pod("pod3") ]
+    deploy >> replicaset >> pods
